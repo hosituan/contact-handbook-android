3a938a7c86d664b95173e07eced5173c2e0b8e08
Võ Hoàng Nguyễn <17110143@student.hcmute.edu.vn>
2021-05-28 09:11:26 +0700

    FeedBack

---
+++ app/src/main/java/com/example/contacthandbook/fragment/feedback/FeedbackAdapter.java
@@ -0,0 +1,113 @@
+package com.example.contacthandbook.fragment.feedback;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.view.ViewGroup;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.contacthandbook.R;
+import com.example.contacthandbook.firebaseManager.FirebaseCallBack;
+import com.example.contacthandbook.firebaseManager.FirebaseManager;
+import com.example.contacthandbook.model.Feedback;
+import com.example.contacthandbook.model.User;
+
+
+import java.util.List;
+
+public class FeedbackAdapter extends RecyclerView.Adapter<FeedbackAdapter.ViewHolder>  {
+
+    private List<Feedback> mData;
+    private LayoutInflater mInflater;
+    private OnItemListener onItemListener;
+    FirebaseManager firebaseManager ;
+    // data is passed into the constructor
+    public FeedbackAdapter(Context context, List<Feedback> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+        firebaseManager = new FirebaseManager(context);
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.feedback_row, parent, false);
+        view.setOnClickListener(new RV_ItemListener());
+        view.setOnLongClickListener(new RV_ItemListener());
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Feedback feedback = mData.get(position);
+        holder.destinationTextView.setText("TO: " + feedback.getReciver());
+        holder.titleTextView.setText("Title: " + feedback.getTitle());
+        holder.messageTextView.setText(feedback.getContent());
+        holder.dateTextView.setText("Date: " + feedback.getDateStr());
+        firebaseManager.getUserName(feedback.getSender(), new FirebaseCallBack.SingleUser() {
+            @Override
+            public void onCallback(User user) {
+                holder.sender.setText("From: " + user.getName());
+            }
+        });
+
+        holder.itemView.setId(position);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder  {
+        TextView destinationTextView;
+        TextView titleTextView;
+        TextView messageTextView;
+        TextView dateTextView;
+        TextView sender;
+        ViewHolder(View itemView) {
+            super(itemView);
+            destinationTextView = itemView.findViewById(R.id.destination);
+            titleTextView = itemView.findViewById(R.id.title);
+            messageTextView = itemView.findViewById(R.id.message);
+            dateTextView = itemView.findViewById(R.id.date);
+            sender = itemView.findViewById(R.id.sender);
+        }
+
+    }
+
+    public  interface OnItemListener{
+        void OnItemClickListener(View view, int position);
+        void OnItemLongClickListener(View view, int position);
+    }
+
+    class RV_ItemListener implements View.OnClickListener, View.OnLongClickListener{
+
+        @Override
+        public void onClick(View view) {
+            if (onItemListener != null){
+                onItemListener.OnItemClickListener(view, view.getId());
+            }
+        }
+        @Override
+        public boolean onLongClick(View view) {
+            if (onItemListener != null){
+                onItemListener.OnItemLongClickListener(view,view.getId());
+            }
+            return true;
+        }
+    }
+
+    public void setOnItemListenerListener(OnItemListener listener){
+        this.onItemListener = listener;
+    }
+
+}
+
---
+++ app/src/main/java/com/example/contacthandbook/model/Feedback.java
@@ -0,0 +1,59 @@
+package com.example.contacthandbook.model;
+
+import java.util.Date;
+
+public class Feedback {
+    String title = "";
+    String content;
+    String reciver;
+    String reciverID;
+    String sender;
+    String dateStr = "";
+
+    Date date;
+    public Feedback(){
+
+    }
+
+    public Feedback(String title,String content,String reciver,String sender,String ReciverID){
+        this.title = title;
+        this.content = content;
+        this.reciver = reciver;
+        this.sender = sender;
+        this.dateStr = new Date().toString();
+        this.reciverID = reciverID;
+    }
+
+    public String getContent(){return content;}
+    public String getReciver(){return reciver;}
+    public void setTeacher(String reciver) {
+        this.reciver = reciver;
+    }
+    public String getSender(){return sender;}
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Date getDate() {
+        return date;
+    }
+
+    public String getDateStr() {
+        return dateStr;
+    }
+
+    public void setReciverID(String reciverID){this.reciverID = reciverID;}
+
+    public String getReciverID(){ return reciverID ;}
+
+    public void setDate(Date date) {
+        this.date = date;
+        this.dateStr = date.toString();
+    }
+}
